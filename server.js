bf22be9fdc738ff69b5b49428f83f82b038ee37c
Fabio <fabio.manzoni@gmail.com>
2024-01-25 14:30:13 +0100

    Changing code

--- server.js
+++ server.js
@@ -1,34 +1,54 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const port = 3000;
+
 const app = express();
-const mediaFolderPath = 'C:/Users/fabio/Music'; // Replace with the path to your media folder
+const port = 3000;
+const mediaFolderPath = './views'; // Replace with the path to your media folder
 
 // Serve media files from the mounted NAS share
 app.use('/media', express.static(mediaFolderPath));
 
-
 // Endpoint to get the list of files
 app.get('/files', (req, res) => {
-    // Read the contents of the media folder
-    fs.readdir(mediaFolderPath, (err, files) => {
-      if (err) {
-        console.error('Error reading media folder:', err);
-        return res.status(500).send('Internal Server Error');
-      }
+  // Read the contents of the media folder
+  fs.readdir(mediaFolderPath, (err, files) => {
+    if (err) {
+      console.error('Error reading media folder:', err);
+      return res.status(500).send('Internal Server Error');
+    }
 
-      // Filter out directories (you may adjust this based on your needs)
-      const fileList = files.filter(file => !fs.statSync(path.join(mediaFolderPath, file)).isDirectory());
+    // Filter out directories (you may adjust this based on your needs)
+    const fileList = files.filter(file => !fs.statSync(path.join(mediaFolderPath, file)).isDirectory());
 
-      // Send the list of files to the client
-      res.json({ files: fileList });
-    });
+    // Send the list of files to the client
+    res.json({ files: fileList });
   });
+});
+
+// Serve the HTML page with clickable links to the files
+app.get('/', (req, res) => {
+  // Read the contents of the media folder
+  fs.readdir(mediaFolderPath, (err, files) => {
+    if (err) {
+      console.error('Error reading media folder:', err);
+      return res.status(500).send('Internal Server Error');
+    }
+
+    // Filter out directories (you may adjust this based on your needs)
+    const fileList = files.filter(file => !fs.statSync(path.join(mediaFolderPath, file)).isDirectory());
+
+    // Generate the HTML for the list of files
+    const html = fileList.map(file => `<a href="/media/${file}">${file}</a>`).join('<br>');
+
+    // Send the HTML to the client
+    res.send(html);
+  });
+});
 
 // ... other routes and middleware
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server listening at the http://localhost:${port}`);
+  console.log('Server is running on port http://localhost:' + port);
 });
